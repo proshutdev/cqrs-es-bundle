720f80c0a9f2113d1cbc118c44564b2aa07f0be6
Hamid <hamid.udc@gmail.com>
2019-06-08 17:01:45 +0430

    added usleep to event dispatcher iterator

--- src/Handler/CommandHandlerTrait.php
+++ src/Handler/CommandHandlerTrait.php
@@ -43,6 +43,7 @@ trait CommandHandlerTrait {
      */
     private function dispatchEvents( EventRecorderInterface $aggregateRoot ) {
         foreach ($aggregateRoot->getRecordedEvents() as $event) {
+            usleep(100);
             $this->eventBus->dispatch($event);
         }
     }
