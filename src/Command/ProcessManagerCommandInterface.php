a72bf37e4b2e067e846b4327b323808a5ff3afea
Hamid <hamid.udc@gmail.com>
2019-06-11 12:37:57 +0430

    Added Proceesmanager

---
+++ src/Command/ProcessManagerCommandInterface.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* This file is part of the Proshut application project
+* (c) Proshut Web Development Company <info@proshut.biz> | 6/11/19
+* For the full copyright and license information, please view the LICENSE
+*/
+
+namespace Proshut\CQRSBundle\Command;
+/**
+ * Interface ProcessManagerCommandInterface
+ * @package Proshut\CQRSBundle\Command
+ */
+interface ProcessManagerCommandInterface {
+
+}
